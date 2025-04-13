5fc0a648fedad88df9ffbb92c780678dd055caa1
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-04-13 17:27:49 +0700

    modif array

--- array/create-read-array.php
+++ array/create-read-array.php
@@ -1,5 +1,26 @@
 <?php
 
-echo("tess dulu gak sih")
+$arrayAngka = [1,2,3,4,5,6,];
+$isiRandom = ["contoh string", false, 325];
+
+function baris(){
+   echo("------------------------\n");
+}
+
+// menampilkan array menggunakan var_dump() atau print_r()
+baris(); // pemisahan
+var_dump($isiRandom);
+baris(); // pemisahan
+var_dump($arrayAngka);
+baris(); // pemisahan
+print_r($isiRandom);
+baris(); // pemisahan
+print_r($arrayAngka);
+baris(); // pemisahan
+
+// menampilkan terpisah
+echo $arrayAngka[3];
+echo $isiRandom[0];
+
 
 ?>
